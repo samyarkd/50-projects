f4b02d86a541497b8b6e2605af39ffd418bb6017
samyar <samyarkd@gmail.com>
2022-01-13 17:13:23 +0330

    Search Input

---
+++ pr-4/script.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("search");
+const btn = document.querySelector(".btn");
+const search = document.querySelector(".search");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus;
+});
