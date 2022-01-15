35023d953d54b3def08d3e34a93481665547717d
samyar <samyarkd@gmail.com>
2022-01-15 18:58:03 +0330

    Scroll animation

---
+++ pr-6/script.js
@@ -0,0 +1,20 @@
+const boxList = document.querySelectorAll('.content')
+
+window.addEventListener('scroll', boxCheck)
+
+boxCheck()
+
+function boxCheck() {
+    const triggerBottom = window.innerHeight - 100
+    
+    boxList.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+            
+        }
+    })
+}
