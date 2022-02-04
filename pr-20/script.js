aedfa0c0d81074509bc4cdcf4a51ebbdf91b75ae
samyar <samyarkd@gmail.com>
2022-02-04 22:17:01 +0330

    Ripple Btn

---
+++ pr-20/script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", async function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const btnX = e.target.offsetLeft;
+    const btnY = e.target.offsetTop;
+
+    const xIn = x - btnX;
+    const yIn = y - btnY;
+
+    const ripple = document.createElement("span");
+    ripple.classList.add("ripple");
+    ripple.style.top = yIn + "px";
+    ripple.style.left = xIn + "px";
+
+    this.appendChild(ripple);
+    await setTimeout( () => ripple.remove(), 500);
+  });
+});
