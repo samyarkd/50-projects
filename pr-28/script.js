c8538514606dc39b6bf3d8edf1ec91e86b3dc2e4
samyar <samyarkd@gmail.com>
2022-02-12 19:53:25 +0330

    Github Profile Finder

--- pr-28/script.js
+++ pr-28/script.js
@@ -1,20 +1,47 @@
-const button = document.getElementById("btn");
-const toasts = document.getElementById("toasts");
+const search = document.getElementById("search");
 
-button.addEventListener("click", showToast);
+const wrp = document.querySelector(".wrapper");
 
-function showToast() {
-  const notif = document.createElement("div");
-  notif.classList.add("toast");
-  notif.innerText = "I am a toast";
-  toasts.appendChild(notif);
-  setTimeout(() => notif.classList.add("show"), 0);
+const prof = document.createElement("div");
+prof.classList.add("profile");
 
-  setTimeout(() => deleteToast(notif), 3000);
+async function fetchData(user) {
+  const res = await fetch(`https://api.github.com/users/${user}`, {
+    method: "GET",
+  });
+  if (res.status === 200) {
+    const data = await res.json();
+
+    prof.innerHTML = `
+        <div class="avatar">
+            <img src="${data.avatar_url}">
+        </div>
+        <div class="user-data">
+            <span class="name">${data.name}</span>
+            <span class="user-name">${data.login}</span>
+            <span class="bio">${data.bio}</span>
+            <div class="sm-data">
+                <span class="followers">Followers : <b>${data.followers}</b></span>
+                <span class="followings">Followings : <b>${data.following}</b></span>
+                <span class="repos">Repositories : <b>${data.public_repos}</b></span>
+            </div>
+            <span class="page"><a href="${data.html_url}">Profile Page</a></span>
+        </div>
+      `;
+    wrp.appendChild(prof);
+  } else {
+    prof.innerHTML = `
+    <div>Page Not Found</div>
+    `;
+    wrp.appendChild(prof);
+  }
 }
 
-function deleteToast(notif) {
-  notif.classList.remove("show");
-  notif.style.marginBottom = '-2.2rem'
-  notif.addEventListener("transitionend", () => notif.remove());
-}
+let username = "";
+search.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    // Fetch Data
+    username = e.target.value;
+    fetchData(username);
+  }
+});
